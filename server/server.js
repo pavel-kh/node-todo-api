b7c45b8c402e7ffe303fbecc3ee36b5c81fa4fff
Pavel Kharakh <no@reply>
2018-02-11 23:00:09 +0200

    Some changes

---
+++ server/server.js
@@ -0,0 +1,24 @@
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./modules/todo');
+var { User } = require('./modules/user');
+var bodyParser = require('body-parser');
+const express = require('express');
+var app = express();
+app.use(bodyParser.json());
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('App started on port 3000');
+
+});
