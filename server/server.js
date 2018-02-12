8d8f4dd6f049520efdc3b215ecaf2558dc6177dd
Pavel Kh <pavelkh@gmail.com>
2018-02-12 19:50:13 +0200

    GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 var { Todo } = require('./modules/todo');
 // var { User } = require('./modules/user');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 const express = require('express');
 var app = express();
 app.use(bodyParser.json());
@@ -18,10 +19,26 @@ app.post('/todos', (req, res) => {
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
-        res.send({todos});
+        res.send({ todos });
     }, (e) => {
         res.send(e);
     });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({ todo });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+
 });
 
 app.listen(3000, () => {
